9e7ad51494ccfd40e50651bac066f2813e6c96c1
SShinMJ <winny0678@gmail.com>
2022-06-30 19:27:45 +0900

    Project setting

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# -Back-Korean-Restaurant-JI
+# [BackEnd]Korean-Restaurant-JI
 한식 전문점 JI BackEnd
+
+기본 url : http://localhost:8080/ <br>
+Swagger : http://localhost:8080/swagger-ui.html
---
+++ src/main/java/com/example/koreanrestaurantji/KoreanRestaurantJiBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.koreanrestaurantji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreanRestaurantJiBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoreanRestaurantJiBackApplication.class, args);
+    }
+
+}
