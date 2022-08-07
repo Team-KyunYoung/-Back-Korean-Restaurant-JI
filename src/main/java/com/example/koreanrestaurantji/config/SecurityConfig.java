102c9c880e8cb13730a3a371b3375838adcf8925
SShinMJ <winny0678@gmail.com>
2022-08-07 19:18:21 +0900

    Feat: Jwt Token Auth

--- src/main/java/com/example/koreanrestaurantji/config/SecurityConfig.java
+++ src/main/java/com/example/koreanrestaurantji/config/SecurityConfig.java
@@ -1,17 +1,24 @@
 package com.example.koreanrestaurantji.config;
 
+import com.example.koreanrestaurantji.util.JwtAuthenticationFilter;
+import com.example.koreanrestaurantji.util.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor //final이 붙거나 @NotNull 이 붙은 필드의 생성자를 자동 생성해주는 롬복 어노테이션(Autowired 안써도됨)
 @Configuration //Bean을 등록함을 의미. (설정파일임을 의미)
 public class SecurityConfig{
+
+    private final JwtTokenProvider jwtTokenProvider;
 
     // Spring Security PWD 암호화
     @Bean
@@ -19,6 +26,16 @@ public class SecurityConfig{
         // BCryptPasswordEncoder : BCrypt 해싱 함수(BCrypt hashing function)를 사용해서 비밀번호를 인코딩해주는 메서드와
         // 사용자의 의해 제출된 비밀번호와 저장소에 저장되어 있는 비밀번호의 일치 여부를 확인해주는 메서드를 제공
         return new BCryptPasswordEncoder();
+    }
+
+    //AuthenticationManager :
+    //사용자 아이디 / 비밀번호 인증을 처리하는 곳  -> 유효한 인증인지 확인
+    //인자로 받은 Authentication이 유효한 인증인지 확인하고,  "Authentication" 객체를 리턴
+    // >  Authentication Manager은 인자로 받은 Authentication을 Provider을 통해, 유효한지 처리하여, "Authentication" 객체를 리턴한다.
+    //Authentication : 인자로 받은 필요한 정보 (username, password , jwt 등..)로 만든 객체
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     //Spring Security 인증 확인 (SecurityFilterChain, WebSecurityCustomizer)
@@ -34,7 +51,9 @@ public class SecurityConfig{
                 .antMatchers("/exception/**", "/item/**", "/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll() // For Swagger
                 .antMatchers("/api/user/signup/**", "/api/user/login/**", "/api/user/checknickname", "/api/user/signup/emailAuth", "/api/user/find/emailAuth").permitAll() // 로그인, 회원가입은 누구나 접근 가능
                 .antMatchers("/api/user/find/**", "/api/user/logout/**", "/api/user/update/nickname", "/api/user/update/password", "/api/user/delete/**").permitAll() // jwt 구현되면 삭제 필요. 토큰 필요한 기능들.
-                .anyRequest().authenticated(); //이 외에는 인증이 필요하도록 한다.
+                .anyRequest().authenticated() //이 외에는 인증이 필요하도록 한다.
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
---
+++ src/main/java/com/example/koreanrestaurantji/util/JwtTokenProvider.java
@@ -0,0 +1,90 @@
+package com.example.koreanrestaurantji.util;
+
+import com.example.koreanrestaurantji.service.UserDetailService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+    // JWT 작업 클래스
+    // 토큰 생성, 토큰에서 (유저)정보 추출, 토큰 유효성 확인
+
+    private String secretKey;
+
+    private final UserDetailService userDetailService;
+
+    // 토큰 유효시간 30분
+    private long tokenValidTime = 30 * 60 * 1000L;
+
+    // 객체 초기화, secretKey를 Base64로 인코딩한다.
+    public JwtTokenProvider(@Value("&{security.jwt.token.secret-key}") String secretKey, UserDetailService userDetailService, @Value("${security.jwt.token.expire-length}") long tokenValidTime) {
+        this.secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+        this.tokenValidTime = tokenValidTime;
+        this.userDetailService = userDetailService;
+    }
+
+    // 토큰 생성
+    public String createToken(String userPk) { //email 받음
+        Claims claims = Jwts.claims().setSubject(String.valueOf(userPk)); // JWT payload에 저장되는 정보 단위
+        claims.put("userPk", userPk); // key/ value 쌍으로 저장
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + tokenValidTime); // set Expire Time
+
+        return Jwts.builder()
+                .setClaims(claims)  // sub 설정 (정보 저장)
+                .setIssuedAt(now)   // 토큰 발행 시간 정보
+                .setExpiration(validity) // Set Expire Time
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                // 사용할 암호화 알고리즘과 signature에 들어갈 secret값 세팅
+                .compact();
+    }
+
+    // 토큰에서 값 추출
+    public Long getSubject(String token) {
+        return Long.valueOf(Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject());
+    }
+
+    // Jwt Token에서 UserPK 추출
+    public String getUserPk(String token) {
+        return Jwts.parser().setSigningKey(secretKey)
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    // 인증 성공시 SecurityContextHolder에 저장할 Authentication 객체 생성
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = userDetailService.loadUserByUsername(this.getUserPk(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    // Request의 Header에서 token 값을 가져오는 함수. "X-AUTH-TOKEN" : "TOKEN값'
+    public String resolveToken(HttpServletRequest request) {
+        return request.getHeader("X-AUTH-TOKEN");
+    }
+
+    // Token의 유효성 + 만료 기간 검사
+    public boolean validateToken(String jwtToken) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+}
