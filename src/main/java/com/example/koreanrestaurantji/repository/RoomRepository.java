7384ab31fc8422270092418a4190f94c14ae794b
SShinMJ <winny0678@gmail.com>
2022-08-20 14:51:40 +0900

    Feat: Room and Room Status Service

--- src/main/java/com/example/koreanrestaurantji/repository/RoomRepository.java
+++ src/main/java/com/example/koreanrestaurantji/repository/RoomRepository.java
@@ -1,2 +1,12 @@
-package com.example.koreanrestaurantji.repository;public class RoomRepository {
+package com.example.koreanrestaurantji.repository;
+
+import com.example.koreanrestaurantji.domain.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+    Optional<Room> findByRoomNumber(long roomNumber);
 }
--- src/main/java/com/example/koreanrestaurantji/repository/RoomStatusRepository.java
+++ src/main/java/com/example/koreanrestaurantji/repository/RoomStatusRepository.java
@@ -1,4 +1,16 @@
 package com.example.koreanrestaurantji.repository;
 
-public interface RoomStatusRepository {
+import com.example.koreanrestaurantji.domain.Room;
+import com.example.koreanrestaurantji.domain.RoomStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RoomStatusRepository extends JpaRepository<RoomStatus, Long> {
+    List<RoomStatus> findByRoom(Room room);
+    List<RoomStatus> findByRoomAndReservationDate(Room room, String reservationDate);
+    Optional<RoomStatus> findByRoomAndReservationDateAndReservationTime(Room room, String reservationDate, String reservationTime);
 }
