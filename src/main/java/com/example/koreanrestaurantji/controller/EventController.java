ce4d4846b132a81d58105ee4da446c90bad20d49
SShinMJ <winny0678@gmail.com>
2022-09-10 19:01:28 +0900

    Fix: Event API url

--- src/main/java/com/example/koreanrestaurantji/controller/EventController.java
+++ src/main/java/com/example/koreanrestaurantji/controller/EventController.java
@@ -28,13 +28,13 @@ public class EventController {
                     required = true, dataType = "String", paramType = "header")
     })
     @ApiOperation(value = "Event 게시글 추가", notes = "Event 게시글 데이터를 추가 합니다.")
-    @PostMapping("/create/event")
+    @PostMapping("/create")
     public BaseResponse createEvent(@ApiParam(value = "게시글 데이터를 갖는 객체", required = true) @RequestBody EventCreateRequestDto eventCreateRequestDto) throws Exception {
         return new BaseResponse(BaseResponseCode.OK.getHttpStatus(), BaseResponseCode.OK.getMessage(), eventService.create(eventCreateRequestDto));
     }
 
     @ApiOperation(value = "Event 게시글 전체 확인", notes = "모든 Event 게시글 데이터 목록을 리턴 합니다.")
-    @GetMapping("/find/event")
+    @GetMapping("/find")
     public BaseResponse<List<EventResponseDto>> findEventList() throws Exception {
         return new BaseResponse(BaseResponseCode.OK.getHttpStatus(), BaseResponseCode.OK.getMessage(), eventService.findEventList());
     }
@@ -52,7 +52,7 @@ public class EventController {
                     required = true, dataType = "String", paramType = "header")
     })
     @ApiOperation(value = "Event 게시글 수정", notes = "Event 게시글 제목/내용 데이터를 수정합니다.")
-    @PutMapping("/update/qna/{eventNumber}")
+    @PutMapping("/update/{eventNumber}")
     public BaseResponse updateEventPost(@ApiParam(value = "questionNumber Event 게시글 일련번호", required = true) @PathVariable long eventNumber,
                                       @ApiParam(value = "수정된 게시글 제목과 내용을 담는 객체", required = true) @RequestBody EventUpdateRequestDto eventUpdateRequestDto) throws Exception {
         return new BaseResponse(BaseResponseCode.OK.getHttpStatus(), "요청 성공했습니다.", eventService.updateEventPost(eventNumber, eventUpdateRequestDto));
