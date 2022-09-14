a99a17e35541c487568c340302b0e4ac83097af1
SShinMJ <winny0678@gmail.com>
2022-09-14 16:04:40 +0900

    Feat: Delete expired Order data

--- src/main/java/com/example/koreanrestaurantji/repository/OrderDishRepository.java
+++ src/main/java/com/example/koreanrestaurantji/repository/OrderDishRepository.java
@@ -3,11 +3,19 @@ package com.example.koreanrestaurantji.repository;
 import com.example.koreanrestaurantji.domain.Orders;
 import com.example.koreanrestaurantji.domain.OrderDish;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
 public interface OrderDishRepository extends JpaRepository<OrderDish, Long> {
     List<OrderDish> findByOrders(Orders orders);
+
+    @Transactional
+    @Modifying
+    @Query( value = "delete from ORDER_DISH o where o.orders in :orders")
+    void deleteAllByDate(List<Orders> orders);
 }
--- src/main/java/com/example/koreanrestaurantji/repository/OrderRepository.java
+++ src/main/java/com/example/koreanrestaurantji/repository/OrderRepository.java
@@ -3,8 +3,13 @@ package com.example.koreanrestaurantji.repository;
 import com.example.koreanrestaurantji.domain.Orders;
 import com.example.koreanrestaurantji.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,4 +17,10 @@ import java.util.Optional;
 public interface OrderRepository extends JpaRepository<Orders, Long> {
     Optional<Orders> findByOrderNumber(Long number);
     List<Orders> findByUserOrderByCreatedDateDesc(User user);
+    List<Orders> findAllByCreatedDateBefore(LocalDateTime limitDate);
+
+    @Transactional
+    @Modifying
+    @Query( value = "delete from ORDERS o where o in :orders")
+    void deleteAllByDate(List<Orders> orders);
 }
