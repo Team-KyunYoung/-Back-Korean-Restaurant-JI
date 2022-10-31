f584d278dea1c1ab72685ee6e82cebfed5ba1046
SShinMJ <winny0678@gmail.com>
2022-10-31 18:17:36 +0900

    Feat: Separate application.properties & application-auth.properties

--- src/main/java/com/example/koreanrestaurantji/KoreanRestaurantJiBackApplication.java
+++ src/main/java/com/example/koreanrestaurantji/KoreanRestaurantJiBackApplication.java
@@ -2,8 +2,10 @@ package com.example.koreanrestaurantji;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = { "classpath:application-auth.properties" })
 public class KoreanRestaurantJiBackApplication {
 
     public static void main(String[] args) {
