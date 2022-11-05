753b15bf3d6ea25cc3ed63da240daf5dd3a7ff74
SShinMJ <winny0678@gmail.com>
2022-11-05 16:42:41 +0900

    Fix: Add image in Event response

--- src/main/java/com/example/koreanrestaurantji/dto/event/EventResponseDto.java
+++ src/main/java/com/example/koreanrestaurantji/dto/event/EventResponseDto.java
@@ -11,12 +11,14 @@ import java.time.format.DateTimeFormatter;
 public class EventResponseDto {
     private final Long eventNumber;
     private final String eventTitle;
+    private final String eventImage;
     private final String writeDate;
     private final int eventView;
 
     public EventResponseDto(EventBoard eventBoard){
         this.eventNumber = eventBoard.getEventNumber();
         this.eventTitle = eventBoard.getEventTitle();
+        this.eventImage = eventBoard.getEventImage();
         this.writeDate = eventBoard.getWriteDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
         this.eventView = eventBoard.getEventView();
     }
